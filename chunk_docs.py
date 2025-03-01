3065b70f1d5b5f36c84b7a1255914bd6f699f433
Jim Thompson <jimthompson5802@gmail.com>
2025-03-01 12:01:22 -0500

    Refactor imports to use community versions of OpenAIEmbeddings and DirectoryLoader

--- chunk_docs.py
+++ chunk_docs.py
@@ -1,7 +1,7 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import SQLiteVec
-from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
 import sqlite3
 
 
